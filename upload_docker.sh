bf66927a332e42c3f2724d2148c96e0c8f268cdc
OmarKhalil10 <omar.khalil498@gmail.com>
2023-08-31 22:04:22 +0300

    update upload_docker.sh

--- upload_docker.sh
+++ upload_docker.sh
@@ -3,15 +3,18 @@
 
 # Assumes that an image is built via `run_docker.sh`
 
+# Set version tag
+version="v2"
+
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
-dockerpath="omarkhalil10/my-docker-api:v1.0.0"
+dockerpath="omarkhalil10/my-docker-api:$version"
 
 # Step 2:
 # Authenticate & tag
 docker login
-docker tag my-docker-api $dockerpath
+docker tag my-docker-api:$version $dockerpath
 echo "Docker ID and Image: $dockerpath"
 
 # Step 3:
